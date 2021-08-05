7c082cb131febf12f88c20927bf5ae0787cbd32a
Anurag <anuragawasthi020@gmail.com>
2021-08-05 20:18:19 +0530

    Adding code for day 5 problem https://leetcode.com/problems/stone-game/

---
+++ src/com/august/weekone/StoneGame.java
@@ -0,0 +1,17 @@
+package com.august.weekone;
+
+public class StoneGame {
+
+	public static void main(String[] args) {
+		int[] piles = new int[] { 5, 3, 4, 5 };
+
+		System.out.println(stoneGame(piles));
+	}
+
+	public static boolean stoneGame(int[] piles) {
+		return true;
+	}
+
+}
+
+//Alex will always win the game.
